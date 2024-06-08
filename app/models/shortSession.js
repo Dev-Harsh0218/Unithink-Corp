b3020a392bd3276801d78ba183c406e35aff6b8f
DevHarsh <mailharsh0218@gmail.com>
2024-06-08 11:59:49 +0530

    deploy issue check

--- app/models/shortSession.js
+++ app/models/shortSession.js
@@ -3,14 +3,17 @@ const Schema = mongoose.Schema;
 
 const shortSessSchema = new Schema(
   {
-    sess_id: { type: String, required: true },
-    c_name: { type: String, required: true },
-    c_email: { type: String, required: true },
-    c_phone: { type: String, required: true },
-    c_course: { type: String, required: true },
-    c_pref_country: { type: String, required: true },
-    type:{type:String, default:"short"},
+    sess_id: { type: String, required:true },
+    c_name: { type: String, required:true },
+    c_email: { type: String, required:true },
+    c_country: { type: String, required:true },
+    c_phone: { type: String, required:true },
+    c_: { type: String, required:true },
+    study_plan:{type:String, required:true},
+    fund_source:{type:String, required:true},
+    study_level:{type:String, required:true},
     date:{type:String, required:true},
+    type:{type:String, default:"Short"},
   }
 );
 
