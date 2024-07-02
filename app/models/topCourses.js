e512f2af109e5d203edc0b66bce5812dae1f7b3f
Harsh Bhardwaj <harsh.bhardwaj@appsuccessor.com>
2024-07-03 01:23:58 +0530

    topCourses bug fixes

--- app/models/topCourses.js
+++ app/models/topCourses.js
@@ -5,7 +5,6 @@ const TopCourseSchema = new Schema({
     course_name: { type: String, required: true },
     countries: [{
     name: { type: String, required: true },
-    details: [{ type: String }],
     }],
     country_conclusion: { type: String, required: true },
     career_opportunities: [{ type: String, required: true }],
